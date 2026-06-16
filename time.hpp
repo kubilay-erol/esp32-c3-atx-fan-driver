83b51d45f910b3c6041c94feb90d57f884213f30
Kubilay Erol <kubilay-erol@outlook.com>
2026-06-16 22:39:18 +0300

    Add Time class for timer functionality

---
+++ time.hpp
@@ -0,0 +1,54 @@
+
+#pragma once
+#include <cstdint>
+#include "esp_timer.h"
+constexpr uint64_t num = 3;
+
+class Time {
+
+public:
+    uint64_t reference[num];
+
+    uint64_t get_timer() {
+      
+        *(reinterpret_cast<volatile uint32_t*>(0x60023004)) = (1 << 30);   
+
+        while ((*(reinterpret_cast<volatile uint32_t*>(0x60023004)) & (1 << 29)) == 0) {
+       
+        }
+
+        uint32_t low = *(reinterpret_cast<volatile uint32_t*>(0x60023044)); 
+        uint32_t high = *(reinterpret_cast<volatile uint32_t*>(0x60023040)); 
+
+        uint64_t ticks = (static_cast<uint64_t>(high) << 32) | low;
+
+        return ticks;
+    
+    }
+
+    uint64_t get_seconds(uint64_t micsec) {
+        return micsec/1'000'000;
+    }
+
+    void set_ref(uint64_t n) {
+        reference[n] = get_timer();
+    }      
+
+    uint64_t get_time(uint64_t n) {
+        return (get_timer() - reference[n]);
+    }
+
+    bool is_delay_complete(uint64_t delay_seconds, uint64_t n) {
+
+        if (get_time(n) >= delay_seconds) {
+            return true; 
+        }
+        return false;
+}
+};
+
+
+
+
+
+   
